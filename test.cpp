acf8d0fe4fec1196a0626ef7a6569ca75d745f61
veona <53961593+veona@users.noreply.github.com>
2019-08-10 10:40:17 +0530

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,22 +1,2 @@
 
-#include<iostream>
-#include<stdio.h>
-using namespace std;
-int main()
-{
-	int n;
-	printf("enter number");
-	cin>>n;
-	int a[n];
-	cout<<"enter no";
-	for(int i=0;i<n;i++)
-	{
-		cin>>a[i];
-	}
-	int max=a[0];
-	for(int i=0;i<n;i++)
-		if(a[i]>max)
-			max=a[i];
-			cout<<"max"<<max;
-			return 0;
-}
+#incl
