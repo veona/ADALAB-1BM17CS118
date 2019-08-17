4f267ec3144b11b5918bbf572376d797458ac6ce
veona <53961593+veona@users.noreply.github.com>
2019-08-17 09:56:21 +0530

    Add files via upload

---
+++ sq.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,s=0;
+	cout<<"enter the no\n";
+	cin>>n;
+	int x=0,y=n/2,mid;
+	while(x<=y)
+	{
+		mid=(x+y)/2;
+		if(mid*mid==n)
+		{
+			s=mid;
+			break;
+		}
+		else if(mid*mid<n)
+		{
+			x=mid+1;
+		}
+		else
+			y=mid-1;
+			
+	}
+	cout<<"square root\n";
+	if(s==1)
+		cout<<x-1;
+		else
+			cout<<s;
+			return 0;
+}
