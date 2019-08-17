bd504bd7208194eae7bde03004b5dfb089fa16de
veona <53961593+veona@users.noreply.github.com>
2019-08-17 10:10:15 +0530

    Add files via upload

---
+++ int.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(int argc,char **argv)
+{
+	int t;
+	cout<<"enter the no of test";
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+		int n,k,f=0;
+		cout<<"enter no of integers";
+		cin>>n;
+		cout<<"enter integers to be found";
+		cin>>k;
+		int ar[n]={0};
+		cout<<"enter integers";
+		for(int j=0;j<n;j++)
+			cin>>ar[j];
+		int b=0,e=n-1,m=(e+b)/2;
+		while(b<e)
+		{
+			if(ar[m]==k)
+				{cout<<"1";
+				f=1; 
+				break;
+				}
+				else if(ar[m]>k)
+					e=m-1;
+				else
+						b=m+1;
+					m=(e+b)/2;
+		}
+		if(f==0)
+			cout<<"1";
+	}
+	return 0;
+}
